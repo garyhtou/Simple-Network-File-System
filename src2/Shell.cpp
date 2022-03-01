2c3e9edf126065323beeaafa5e687c000ac82d8f
Gary Tou <gary@garytou.com>
2022-03-01 03:05:47 -0800

    Format server + shell files

--- src2/Shell.cpp
+++ src2/Shell.cpp
@@ -8,71 +8,84 @@ using namespace std;
 
 #include "Shell.h"
 
-static const string PROMPT_STRING = "NFS> ";	// shell prompt
+static const string PROMPT_STRING = "NFS> "; // shell prompt
 
 // Mount the network file system with server name and port number in the format of server:port
-void Shell::mountNFS(string fs_loc) {
-	//create the socket cs_sock and connect it to the server and port specified in fs_loc
-	//if all the above operations are completed successfully, set is_mounted to true  
+void Shell::mountNFS(string fs_loc)
+{
+  // create the socket cs_sock and connect it to the server and port specified in fs_loc
+  // if all the above operations are completed successfully, set is_mounted to true
 }
 
 // Unmount the network file system if it was mounted
-void Shell::unmountNFS() {
-	// close the socket if it was mounted
+void Shell::unmountNFS()
+{
+  // close the socket if it was mounted
 }
 
 // Remote procedure call on mkdir
-void Shell::mkdir_rpc(string dname) {
+void Shell::mkdir_rpc(string dname)
+{
   // to implement
 }
 
 // Remote procedure call on cd
-void Shell::cd_rpc(string dname) {
+void Shell::cd_rpc(string dname)
+{
   // to implement
 }
 
 // Remote procedure call on home
-void Shell::home_rpc() {
+void Shell::home_rpc()
+{
   // to implement
 }
 
 // Remote procedure call on rmdir
-void Shell::rmdir_rpc(string dname) {
+void Shell::rmdir_rpc(string dname)
+{
   // to implement
 }
 
 // Remote procedure call on ls
-void Shell::ls_rpc() {
+void Shell::ls_rpc()
+{
   // to implement
 }
 
 // Remote procedure call on create
-void Shell::create_rpc(string fname) {
+void Shell::create_rpc(string fname)
+{
   // to implement
 }
 
 // Remote procedure call on append
-void Shell::append_rpc(string fname, string data) {
+void Shell::append_rpc(string fname, string data)
+{
   // to implement
 }
 
 // Remote procesure call on cat
-void Shell::cat_rpc(string fname) {
+void Shell::cat_rpc(string fname)
+{
   // to implement
 }
 
 // Remote procedure call on head
-void Shell::head_rpc(string fname, int n) {
+void Shell::head_rpc(string fname, int n)
+{
   // to implement
 }
 
 // Remote procedure call on rm
-void Shell::rm_rpc(string fname) {
+void Shell::rm_rpc(string fname)
+{
   // to implement
 }
 
 // Remote procedure call on stat
-void Shell::stat_rpc(string fname) {
+void Shell::stat_rpc(string fname)
+{
   // to implement
 }
 
@@ -81,11 +94,12 @@ void Shell::run()
 {
   // make sure that the file system is mounted
   if (!is_mounted)
- 	return; 
-  
+    return;
+
   // continue until the user quits
   bool user_quit = false;
-  while (!user_quit) {
+  while (!user_quit)
+  {
 
     // print prompt and get command line
     string command_str;
@@ -105,21 +119,22 @@ void Shell::run_script(char *file_name)
 {
   // make sure that the file system is mounted
   if (!is_mounted)
-  	return;
+    return;
   // open script file
   ifstream infile;
   infile.open(file_name);
-  if (infile.fail()) {
+  if (infile.fail())
+  {
     cerr << "Could not open script file" << endl;
     return;
   }
-
 
   // execute each line in the script
   bool user_quit = false;
   string command_str;
   getline(infile, command_str, '\n');
-  while (!infile.eof() && !user_quit) {
+  while (!infile.eof() && !user_quit)
+  {
     cout << PROMPT_STRING << command_str << endl;
     user_quit = execute_command(command_str);
     getline(infile, command_str);
@@ -130,7 +145,6 @@ void Shell::run_script(char *file_name)
   infile.close();
 }
 
-
 // Executes the command. Returns true for quit and false otherwise.
 bool Shell::execute_command(string command_str)
 {
@@ -138,51 +152,67 @@ bool Shell::execute_command(string command_str)
   struct Command command = parse_command(command_str);
 
   // look for the matching command
-  if (command.name == "") {
+  if (command.name == "")
+  {
     return false;
   }
-  else if (command.name == "mkdir") {
+  else if (command.name == "mkdir")
+  {
     mkdir_rpc(command.file_name);
   }
-  else if (command.name == "cd") {
+  else if (command.name == "cd")
+  {
     cd_rpc(command.file_name);
   }
-  else if (command.name == "home") {
+  else if (command.name == "home")
+  {
     home_rpc();
   }
-  else if (command.name == "rmdir") {
+  else if (command.name == "rmdir")
+  {
     rmdir_rpc(command.file_name);
   }
-  else if (command.name == "ls") {
+  else if (command.name == "ls")
+  {
     ls_rpc();
   }
-  else if (command.name == "create") {
+  else if (command.name == "create")
+  {
     create_rpc(command.file_name);
   }
-  else if (command.name == "append") {
+  else if (command.name == "append")
+  {
     append_rpc(command.file_name, command.append_data);
   }
-  else if (command.name == "cat") {
+  else if (command.name == "cat")
+  {
     cat_rpc(command.file_name);
   }
-  else if (command.name == "head") {
+  else if (command.name == "head")
+  {
     errno = 0;
     unsigned long n = strtoul(command.append_data.c_str(), NULL, 0);
-    if (0 == errno) {
+    if (0 == errno)
+    {
       head_rpc(command.file_name, n);
-    } else {
+    }
+    else
+    {
       cerr << "Invalid command line: " << command.append_data;
       cerr << " is not a valid number of bytes" << endl;
       return false;
     }
   }
-  else if (command.name == "rm") {
+  else if (command.name == "rm")
+  {
     rm_rpc(command.file_name);
   }
-  else if (command.name == "stat") {
+  else if (command.name == "stat")
+  {
     stat_rpc(command.file_name);
   }
-  else if (command.name == "quit") {
+  else if (command.name == "quit")
+  {
     return true;
   }
 
@@ -200,14 +230,18 @@ Shell::Command Shell::parse_command(string command_str)
   struct Command command;
   istringstream ss(command_str);
   int num_tokens = 0;
-  if (ss >> command.name) {
+  if (ss >> command.name)
+  {
     num_tokens++;
-    if (ss >> command.file_name) {
+    if (ss >> command.file_name)
+    {
       num_tokens++;
-      if (ss >> command.append_data) {
+      if (ss >> command.append_data)
+      {
         num_tokens++;
         string junk;
-        if (ss >> junk) {
+        if (ss >> junk)
+        {
           num_tokens++;
         }
       }
@@ -215,30 +249,33 @@ Shell::Command Shell::parse_command(string command_str)
   }
 
   // Check for empty command line
-  if (num_tokens == 0) {
+  if (num_tokens == 0)
+  {
     return empty;
   }
-    
+
   // Check for invalid command lines
   if (command.name == "ls" ||
       command.name == "home" ||
       command.name == "quit")
   {
-    if (num_tokens != 1) {
+    if (num_tokens != 1)
+    {
       cerr << "Invalid command line: " << command.name;
       cerr << " has improper number of arguments" << endl;
       return empty;
     }
   }
   else if (command.name == "mkdir" ||
-      command.name == "cd"    ||
-      command.name == "rmdir" ||
-      command.name == "create"||
-      command.name == "cat"   ||
-      command.name == "rm"    ||
-      command.name == "stat")
+           command.name == "cd" ||
+           command.name == "rmdir" ||
+           command.name == "create" ||
+           command.name == "cat" ||
+           command.name == "rm" ||
+           command.name == "stat")
   {
-    if (num_tokens != 2) {
+    if (num_tokens != 2)
+    {
       cerr << "Invalid command line: " << command.name;
       cerr << " has improper number of arguments" << endl;
       return empty;
@@ -246,18 +283,19 @@ Shell::Command Shell::parse_command(string command_str)
   }
   else if (command.name == "append" || command.name == "head")
   {
-    if (num_tokens != 3) {
+    if (num_tokens != 3)
+    {
       cerr << "Invalid command line: " << command.name;
       cerr << " has improper number of arguments" << endl;
       return empty;
     }
   }
-  else {
+  else
+  {
     cerr << "Invalid command line: " << command.name;
-    cerr << " is not a command" << endl; 
+    cerr << " is not a command" << endl;
     return empty;
-  } 
+  }
 
   return command;
 }
-
