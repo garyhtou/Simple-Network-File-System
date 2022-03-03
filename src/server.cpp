e3a6fb9be607a97b3d2a7e833becb84cc88293a1
Gary Tou <gary@garytou.com>
2022-03-02 19:20:38 -0800

    Add `CommandType` enum for command parsing
    Co-authored-by: Castel Villalobos <impropernoun@users.noreply.github.com>

--- src/server.cpp
+++ src/server.cpp
@@ -5,35 +5,40 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <sstream>
 #include "FileSys.h"
 using namespace std;
 
-const int BACKLOG = 5;       // Silently max value defined by C.
-const int BUFFER_LEN = 2048; // TODO: why?
+const int BACKLOG = 5; // Silent max value defined by C.
 
 // struct to hold command lines
+enum CommandType
+{
+    mkdir,
+    ls,
+    cd,
+    home,
+    rmdir_cmd, // Prevent name colision with an existing `rmdir` function.
+    create,
+    append,
+    stat,
+    cat,
+    head,
+    rm,
+    quit
+};
 struct Command
 {
-    commands name; // string to hold the function cmd.name
-    string file;   // cmd.name of the file to manipulate
-    string data;   // data to add or for the append or number for head
-}
-
-enum commands { ls = "ls",
-                cd = "cd",
-                home = "home",
-                rmdir = "rmdir",
-                create = "create",
-                append = "append",
-                stat = "stat",
-                cat = "cat",
-                head = "head",
-                rm = "rm" };
+    CommandType type; // string to hold the function cmd.name
+    string file;      // cmd.name of the file to manipulate
+    string data;      // data to add or for the append or number for head
+};
 
 // Forward declaration
-sockaddr_in
-get_server_addr(in_port_t port);
-void parse_call(string message);
+sockaddr_in get_server_addr(in_port_t port);
+Command parse_command(string message);
+void exec_command(FileSys &fs, Command command);
+void response_error(string message);
 
 int main(int argc, char *argv[])
 {
@@ -84,7 +89,7 @@ int main(int argc, char *argv[])
                           // for a TCP connection between the client and the server.
 
     // Receive the message
-    string message;        // string that acts as abuffer
+    string message;        // Will contain the command from the client
     char temp_buff[65535]; // max packet size
     int retries_left = 3;
     while (true)
@@ -107,19 +112,27 @@ int main(int argc, char *argv[])
             // End of message
             break;
         }
+        else
+        {
+            // Successful read data. Reset the number of retries left
+            retries_left = 3;
+        }
 
         // Process the packet
         for (int i = 0; i < recv_ret; i++)
         {
-            message.append(message[i]);
+            message += temp_buff[i];
         }
     }
 
+    // Parse the command
+    Command command = parse_command(message);
     // Execute the command
-    exec_call(message);
+    exec_command(fs, command);
 
-    // close the listening socket
+    // close the listening sockets
     close(new_sockfd);
+    close(sockfd);
 
     // unmout the file system
     fs.unmount();
@@ -135,40 +148,82 @@ sockaddr_in get_server_addr(in_port_t port)
     server_addr.sin_addr.s_addr = INADDR_ANY;
     // sets all bytes of sin_zero (which is not used)
     // to the null terminatior
-    for (int i = 0; i < sizeof(8); i++)
+    for (int i = 0; i < 8; i++)
     {
         server_addr.sin_zero[i] = '\0';
     }
     return server_addr;
 }
 
-void exec_call(string message)
+void exec_command(FileSys &fs, Command command)
 {
-    Command command = parse_call(message);
+    CommandType type = command.type;
+    const char *file = command.file.c_str();
+    const char *data = command.data.c_str();
 
     try
     {
-        switch ()
+        // FileSys will respond to the socket on a successful request. If there
+        // is an error, then a WrappedFileSystem::FileSystemException will be
+        // thrown by FileSys and this function will handle and respond to the
+        // socket (below in `catch`).
+        switch (type)
+        {
+        case mkdir:
+            fs.mkdir(file);
+            break;
+        case ls:
+            fs.ls();
+            break;
+        case cd:
+            fs.cd(file);
+            break;
+        case home:
+            fs.home();
+            break;
+        case rmdir_cmd:
+            fs.rmdir(file);
+            break;
+        case create:
+            fs.create(file);
+            break;
+        case append:
+            fs.append(file, data);
+            break;
+        case stat:
+            fs.stat(file);
+            break;
+        case cat:
+            fs.cat(file);
+            break;
+        case head:
+            fs.head(file, stoi(data));
+            break;
+        case rm:
+            fs.rm(file);
+            break;
+        }
     }
     catch (const WrappedFileSys::FileSystemException &e)
     {
         string err_msg = e.what();
         // Response to socket with err_msg in proper format
+        // TODO:
     }
 }
 
 // Takes the buffer string and calls the
 // appropriate function from FileSys
-struct Command parse_call(string message)
+Command parse_command(string message)
 {
-
-    // parse the buffer string
+    // Parse the buffer string
     struct Command cmd;
     istringstream ss(message);
+    string name;
     int tokens = 0; // number of tokens
 
     // get tokens and determine number
-    if (ss >> cmd.name)
+    if (ss >> name)
     {
         tokens++;
         if (ss >> cmd.file)
@@ -178,8 +233,8 @@ struct Command parse_call(string message)
         if (ss >> cmd.data)
         {
             tokens++;
-            string garbage; // data to ignore
         }
+        string garbage; // data to ignore
         if (ss >> garbage)
         {
             tokens++;
@@ -187,47 +242,103 @@ struct Command parse_call(string message)
     }
 
     // empty command line handle
-    if (num_tokens == 0)
+    if (tokens == 0)
     {
         // TODO: send back error message saying empty/no command provided
-        exit(1);
+        exit(0);
     }
 
+    // Convert the name to a CommandType
+    if (name == "mkdir")
+    {
+        cmd.type = mkdir;
+    }
+    else if (name == "ls")
+    {
+        cmd.type = ls;
+    }
+    else if (name == "cd")
+    {
+        cmd.type = cd;
+    }
+    else if (name == "home")
+    {
+        cmd.type = home;
+    }
+    else if (name == "rmdir")
+    {
+        cmd.type = rmdir_cmd;
+    }
+    else if (name == "create")
+    {
+        cmd.type = create;
+    }
+    else if (name == "append")
+    {
+        cmd.type = append;
+    }
+    else if (name == "stat")
+    {
+        cmd.type = stat;
+    }
+    else if (name == "cat")
+    {
+        cmd.type = cat;
+    }
+    else if (name == "head")
+    {
+        cmd.type = head;
+    }
+    else if (name == "rm")
+    {
+        cmd.type = rm;
+    }
+    else
+    {
+        // Invalid command type
+        // TODO:
+        exit(0);
+    }
+
+    CommandType type = cmd.type;
+
     // if command lines have too many args
-    if (cmd.name == "ls" || cmd.name == "home" || cmd.name == "quit")
+    if (type == ls || type == home || type == quit)
     {
         if (tokens != 1)
         {
             // TODO: send error message "Invalide command: not enough args"
-            exit(1);
+            exit(0);
         }
     } // otherwise check for too few arguments
-    else if (cmd.name == "mkdir" ||
-             cmd.name == "cd" ||
-             cmd.name == "rmdir" ||
-             cmd.name == "create" ||
-             cmd.name == "cat" ||
-             cmd.name == "rm" ||
-             cmd.name == "stat")
+    else if (type == mkdir ||
+             type == cd ||
+             type == rmdir_cmd ||
+             type == create ||
+             type == cat ||
+             type == rm ||
+             type == stat)
     {
-        if (num_tokens != 2)
+        if (tokens != 2)
         {
             // TODO: error handling
-            exit(1);
+            exit(0);
         }
     }
     // too few args for append or head
-    else if (cmd.name == "append" || cmd.name == "head")
+    else if (type == append || type == head)
     {
-        if (num_tokens != 3)
+        if (tokens != 3)
         {
             // TODO: error handling
-            exit(1)
+            exit(0);
         }
     }
-    else
-    {
-        // TODO: invalid command
-        exit(1);
-    }
+
+    return cmd;
+}
+
+void response_error(string message)
+{
+    // TODO: send the error message to the client
 }
